ff40630579e68196ec8ea0a0c401e9a990ad5720
Kryštof Rainisch <krystof.rainisch@hey.com>
2026-03-09 18:23:11 +0100

    Add structured mentions array to webhook payload

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -35,7 +35,8 @@ class Webhook < ApplicationRecord
       {
         user:    { id: message.creator.id, name: message.creator.name },
         room:    { id: room.id, name: room_name(room), type: room.type.demodulize.downcase, path: room_bot_messages_path(message) },
-        message: { id: message.id, body: { html: message.body.body, plain: without_recipient_mentions(message.plain_text_body) }, path: message_path(message) }
+        message: { id: message.id, body: { html: message.body.body, plain: without_recipient_mentions(message.plain_text_body) }, path: message_path(message) },
+        mentions: message.mentionees.map { |u| { id: u.id, name: u.name, bot: u.bot? } }
       }.to_json
     end
 
